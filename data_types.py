213ef3d9db1584fb01e941b83e120bdb7c5e468f
Feiyu Lu <feiyu.lu@noaa.gov>
2025-06-16 22:21:09 -0400

    Added code to coarsen model output and Argo profiles

--- data_types.py
+++ data_types.py
@@ -1,11 +1,14 @@
 import numpy as np
+import xarray as xr
+import scipy.interpolate as interpolate
 
 class argo:
     def __init__(
             self,dataset,nread=None,meta_only=False,nstart=0,
             T_name='temp',S_name='salt',depth_name='depth',
             station_index='station_index',depth_index='depth_index',time_name='time',
-            lon_name='longitude',lat_name='latitude',link_name='link'):
+            lon_name='longitude',lat_name='latitude',link_name='link',
+            interp=False,target_depth=None,year=None):
         """Initialize an Argo float dataset."""
         
         links_all=dataset[link_name]
@@ -14,59 +17,70 @@ class argo:
 
         dataset.load()
 
+        if interp:
+            model_z = target_depth[target_depth<2000]
+            self.model_z = model_z
+            self.model_levels = len(model_z)
+            self.interp=interp
+
+        if year:
+            time_start = np.datetime64(f'{year}-01-01T00:00:00')
+            time_end = np.datetime64(f'{year+1}-01-01T00:00:00')
+        else:
+            time_start = np.datetime64('1900-01-01T00:00:00')
+            time_end = np.datetime64('2100-01-01T00:00:00')
+            
         i = nstart
         self.profiles = []
         nread = nobs if (nread is None or nread + nstart > nobs) else nread
 
         while (i < nread + nstart and i < nobs):
-            depth = []
-            T = []
-            S = []
-            lat = dataset[lat_name][i].values
-            lon = dataset[lon_name][i].values
             time = dataset[time_name][i].values
+            if (time_start < time < time_end):
+                depth = []
+                T = []
+                S = []
+                lat = dataset[lat_name][i].values
+                lon = dataset[lon_name][i].values
 
-            depth=dataset[depth_name][i,0:len_chunk].data
-            if meta_only:
-                T=[]
-                S=[]
+                depth=dataset[depth_name][i,0:len_chunk].data
+                if meta_only:
+                    T=[]
+                    S=[]
+                else:
+                    T=dataset[T_name][i,0:len_chunk].data
+                    S=dataset[S_name][i,0:len_chunk].data
+
+                ### link chuncked profiles together
+                while (links_all[i]):
+                    # if links_all[i+1]:
+                    depth=np.concatenate([depth,dataset[depth_name][i+1,0:len_chunk].data])
+                    if not meta_only:
+                        T=np.concatenate([T,dataset[T_name][i+1,0:len_chunk].data])
+                        S=np.concatenate([S,dataset[S_name][i+1,0:len_chunk].data])
+                    i += 1
+
+                if (len(depth)!=len(T) or len(depth)!=len(S)):
+                    raise Exception('Argo profile levels do not match')
+
+                ### Remove depths outside of the 0-6500m range (missing/fill data problem)
+                depth_mask=depth[(depth>0) & (depth<6500) & ~np.isnan(T) & ~np.isnan(S)]
+                T_mask=T[(depth>0) & (depth<6500) & ~np.isnan(T) & ~np.isnan(S)]
+                S_mask=S[(depth>0) & (depth<6500) & ~np.isnan(T) & ~np.isnan(S)]
+
+                ### Remove DeepArgo profiles and profiles that didn't reach close to surface
+                if len(depth_mask)>5:
+                    if depth_mask[0]<30 or depth_mask[-1]<2000:
+                        ### Need to subsample(thinning) the profiles with too many levels
+                        if interp:
+                            T_new,S_new = argo_vertical_coarsen(depth_mask,T_mask,S_mask,model_z)
+                            self.profiles.append(argo_float(time,lat,lon,model_z,T_new,S_new,meta_only))
+                        else:
+                            self.profiles.append(argo_float(time,lat,lon,depth_mask,T_mask,S_mask,meta_only))
             else:
-                T=dataset[T_name][i,0:len_chunk].data
-                S=dataset[S_name][i,0:len_chunk].data
+                while (links_all[i]):
+                    i += 1
 
-            ### link chuncked profiles together
-            while (links_all[i]):
-                # if links_all[i+1]:
-                depth=np.concatenate([depth,dataset[depth_name][i+1,0:len_chunk].data])
-                if not meta_only:
-                    T=np.concatenate([T,dataset[T_name][i+1,0:len_chunk].data])
-                    S=np.concatenate([S,dataset[S_name][i+1,0:len_chunk].data])
-                # else:
-                #     depth=np.concatenate(
-                #         [depth,dataset[depth_name][i+1,0:len_chunk].dropna(dim=depth_index).data])
-                #     if not meta_only:
-                #         T=np.concatenate(
-                #             [T,dataset[T_name][i+1,0:len_chunk].dropna(dim=depth_index).data])
-                #         S=np.concatenate(
-                #             [S,dataset[S_name][i+1,0:len_chunk].dropna(dim=depth_index).data])
-                i += 1
-
-            if (len(depth)!=len(T) or len(depth)!=len(S)):
-                raise Exception('Argo profile levels do not match')
-
-            ### Remove depths outside of the 0-6500m range (missing/fill data problem)
-            depth_mask=depth[(depth>0) & (depth<6500)]
-            T_mask=T[(depth>0) & (depth<6500)]
-            S_mask=S[(depth>0) & (depth<6500)]
-
-            ### Remove DeepArgo profiles and profiles that didn't reach close to surface
-            if depth[0]<30 or depth[-1]<2000:
-
-                ### Need to subsample(thinning) the profiles with too many levels
-                # if len(depth) > 100 or (depth[-1]-depth[0])/len(depth):
-                #     depth_new,T_new,S_new = argo_vertical_coarsen(depth,T,S,model_dz)
-
-                self.profiles.append(argo_float(time,lat,lon,depth_mask,T_mask,S_mask,meta_only))
             i += 1
 
         ### Get some summary statistics for easy diagnostics
@@ -76,6 +90,29 @@ class argo:
         self.lat = np.array([profile.lat for profile in self.profiles])
         self.lon = np.array([profile.lon for profile in self.profiles])
         self.time = np.array([profile.time for profile in self.profiles])
+
+    def convert_interpolated_to_dataset(self):
+        if not self.interp:
+            raise Exception("This Argo dataset is not interpolated")
+
+        nprofiles = len(self)
+        profile_index = range(nprofiles)
+        levels = self.model_levels
+        z = self.model_z
+
+        T_data = np.empty((nprofiles,levels))
+        S_data = np.empty((nprofiles,levels))
+
+        for i in range(nprofiles):
+            T_data[i,:] = self.profiles[i].T
+            S_data[i,:] = self.profiles[i].S
+
+        T_var = xr.DataArray(T_data,coords=[profile_index,z],dims=['profile','z'])
+        S_var = xr.DataArray(S_data,coords=[profile_index,z],dims=['profile','z'])
+
+        ds = xr.Dataset(dict(T=T_var,S=S_var))
+
+        return ds
 
     def __len__(self):
         return len(self.profiles)
@@ -126,10 +163,73 @@ class argo_float:
     def get_levels(self):
         return len(self.depth)
 
-def model_vertical_coarsen(T,dz,combine_levels,z_name='z'):
-    ### combine_levels = [0,6,12,18,23,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75]
-    if len(T[z_name]) != len(dz):
+def depth_vertical_coarsen(model_dz,combine_levels):
+
+    if combine_levels[-1] > len(model_dz):
+        raise Exception('Combine levels exceed depth levels')
+
+    model_zi = np.zeros(len(model_dz)+1)
+    for i in range(1,len(model_dz)+1):
+        model_zi[i] = model_dz[0:i].sum().values
+
+    model_z_coarse = np.zeros(len(combine_levels)-1)
+    model_dz_coarse = np.zeros(len(combine_levels)-1)
+    model_zi_coarse = np.zeros(len(combine_levels))
+    for i in range(1,len(combine_levels)):
+        model_dz_coarse[i-1] = model_dz[combine_levels[i-1]:combine_levels[i]].sum().data
+        model_zi_coarse[i] = model_zi[combine_levels[i]]
+        model_z_coarse[i-1] = (model_zi_coarse[i]+model_zi_coarse[i-1])/2
+
+    return model_dz_coarse,model_z_coarse
+
+def variable_vertical_coarsen(Var,model_dz,combine_levels,time_name='time',x_name='xh',y_name='yh',z_name='z'):
+
+    if combine_levels[-1] > len(model_dz):
+        raise Exception('Combine levels exceed depth levels')
+    if len(Var[z_name]) != len(model_dz):
         raise Exception('vertical levels not matched')
-        
-def argo_vertical_coarsen(depth,T,S,model_dz):
-    return depth,T,S
+ 
+    model_z = np.zeros(len(model_dz))
+    model_z[0] = model_dz[0]/2 
+    for i in range(1,len(model_dz)):
+        model_z[i] = model_z[i-1] + (model_dz[i-1]+model_dz[i])/2
+    model_zi = np.zeros(len(model_dz)+1)
+    for i in range(1,len(model_dz)+1):
+        model_zi[i] = model_dz[0:i].sum().values
+  
+    model_z_coarse = np.zeros(len(combine_levels)-1)
+    model_dz_coarse = np.zeros(len(combine_levels)-1)
+    model_zi_coarse = np.zeros(len(combine_levels))
+    for i in range(1,len(combine_levels)):
+        model_dz_coarse[i-1] = model_dz[combine_levels[i-1]:combine_levels[i]].sum().data
+        model_zi_coarse[i] = model_zi[combine_levels[i]]
+        model_z_coarse[i-1] = (model_zi_coarse[i]+model_zi_coarse[i-1])/2
+
+    Var_data_coarse = np.empty((len(Var[time_name]),len(model_dz_coarse),len(Var[y_name]),len(Var[x_name])))
+    Var_weighted = Var*model_dz.data[None,:,None,None]
+    for i in range(len(model_dz_coarse)):
+        Var_data_coarse[:,i,:,:] = \
+            Var_weighted[:,combine_levels[i]:combine_levels[i+1],:,:].sum(dim=z_name,skipna=True)/\
+            model_dz_coarse[i]
+
+    Var_coarse = xr.DataArray(
+        Var_data_coarse,
+        coords=[Var[time_name],model_z_coarse,Var[y_name],Var[x_name]],
+        dims=Var.dims).astype('float32')
+
+    return Var_coarse
+
+def argo_vertical_coarsen(depth,T,S,target_depth):
+
+    T_target = np.empty(len(target_depth))
+    S_target = np.empty(len(target_depth))
+
+    # T_target = np.interp(target_depth,depth,T,left=np.nan,right=np.nan)
+    # S_target = np.interp(target_depth,depth,S,left=np.nan,right=np.nan)
+
+    spl_T = interpolate.CubicSpline(depth,T,extrapolate=False)
+    T_target = spl_T(target_depth)
+    spl_S = interpolate.CubicSpline(depth,S,extrapolate=False)
+    S_target = spl_S(target_depth)
+
+    return T_target, S_target
